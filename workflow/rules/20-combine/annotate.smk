389a8936ab77acc4c86052abfb208674d2a754c4
Peter Ebert <36245614+ptrebert@users.noreply.github.com>
2025-03-07 19:16:48 +0100

    add script to relabel combined annotations

--- workflow/rules/20-combine/annotate.smk
+++ workflow/rules/20-combine/annotate.smk
@@ -16,7 +16,14 @@ if RUN_HMMER:
     _MULTI_ANNOTATION_LABELS.extend(sorted(HMMER_MOTIF_NAMES))
 
 
-if RUN_MINIMAP_REGIONDB:
+if RUN_MINIMAP_REGIONDB and RUN_MINIMAP_LABELREF:
+
+    if any(label in MINIMAP_REGION_DB_NAMES for label in MINIMAP_LABELED_REFERENCE_NAMES):
+        _disjoin_regiondb = [label + ".regiondb" for label in sorted(MINIMAP_REGION_DB_NAMES)]
+        _disjoin_labelref = [label + ".labelref" for label in sorted(MINIMAP_LABELED_REFERENCE_NAMES)]
+    else:
+        _disjoin_regiondb = sorted(MINIMAP_REGION_DB_NAMES)
+        _disjoin_labelref = sorted(MINIMAP_LABELED_REFERENCE_NAMES)
     _MULTI_ANNOTATION_INPUT.extend(
         sorted(
             expand(
@@ -26,15 +33,8 @@ if RUN_MINIMAP_REGIONDB:
             )
         )
     )
-    _MULTI_ANNOTATION_LABELS.extend(
-        [
-            label + ".regiondb" for label in
-            sorted(MINIMAP_REGION_DB_NAMES)
-        ]
-    )
+    _MULTI_ANNOTATION_LABELS.extend(_disjoin_regiondb)
 
-
-if RUN_MINIMAP_LABELREF:
     _MULTI_ANNOTATION_INPUT.extend(
         sorted(
             expand(
@@ -44,12 +44,34 @@ if RUN_MINIMAP_LABELREF:
             )
         )
     )
-    _MULTI_ANNOTATION_LABELS.extend(
-        [
-            label + ".labelref" for label in
-            sorted(MINIMAP_LABELED_REFERENCE_NAMES)
-        ]
+    _MULTI_ANNOTATION_LABELS.extend(_disjoin_labelref)
+
+elif RUN_MINIMAP_REGIONDB
+    _MULTI_ANNOTATION_INPUT.extend(
+        sorted(
+            expand(
+                rules.create_annotation_region_db.output.bed,
+                region_db=MINIMAP_REGION_DB_NAMES,
+                allow_missing=True
+            )
+        )
     )
+    _MULTI_ANNOTATION_LABELS.extend(sorted(MINIMAP_REGION_DB_NAMES))
+
+elif RUN_MINIMAP_LABELREF:
+    _MULTI_ANNOTATION_INPUT.extend(
+        sorted(
+            expand(
+                rules.create_annotation_labeled_ref.output.bed,
+                labelref=MINIMAP_LABELED_REFERENCE_NAMES,
+                allow_missing=True
+            )
+        )
+    )
+    _MULTI_ANNOTATION_LABELS.extend(sorted(MINIMAP_LABELED_REFERENCE_NAMES))
+
+else:
+    pass
 
 
 if _MULTI_ANNOTATION_LABELS:
@@ -74,10 +96,74 @@ rule bedtools_annotation_multi_intersect:
         "bedtools multiinter -header -names {params.header} -i {input.bed_files} | gzip > {output.table}"
 
 
+localrules: dump_annotation_label_listings
+rule dump_annotation_label_listings:
+    input:
+        bed_files = _MULTI_ANNOTATION_INPUT
+    output:
+        lst_files = DIR_PROC.joinpath(
+            "20-combine", "annotate", "multiinter",
+            "{sample}.{path_id}.annot-files.lst"
+        ),
+        lst_labels = DIR_PROC.joinpath(
+            "20-combine", "annotate", "multiinter",
+            "{sample}.{path_id}.annot-labels.lst"
+        ),
+    params:
+        labels = _MULTI_ANNOTATION_LABELS
+    run:
+        n_files = 0
+        with open(output.lst_files) as listing:
+            for filepath in input.bed_files:
+                _ = listing.write(f"{filepath}\n")
+                n_files += 1
+        n_labels = 0
+        with open(output.lst_labels) as listing:
+            for label in params.labels:
+                _ = listing.write(f"{label}\n")
+                n_labels += 1
+        assert n_files == n_labels
+    # END OF RUN BLOCK
+
+
+rule relabel_multi_annotation_table:
+    input:
+        lst_files = rules.dump_annotation_label_listings.output.lst_files,
+        lst_labels = rules.dump_annotation_label_listings.output.lst_labels,
+        isect = rules.bedtools_annotation_multi_intersect.output.table
+    output:
+        tmp_bed = temp(
+            DIR_PROC.joinpath(
+                "20-combine", "annotate", "multiinter", "tmp",
+                "{sample}.{path_id}.relabeled.bed"
+            )
+        ),
+        bed = DIR_RES.joinpath(
+            "annotations", "combined",
+            "{sample}.{path_id}.relabeled.bed.gz"
+        )
+    conda:
+        DIR_ENVS.joinpath("scripts", "pyseq")
+    params:
+        script=find_script("add_annot_labels")
+    shell:
+        "{params.script} -i {input.isect} -a {input.lst_files} -l {input.lst_labels} --out-bed {output.tmp_bed}"
+            " && "
+        "bgzip -c {output.tmp_bed} > {output.bed}"
+            " && "
+        "tabix -p bed {output.bed}"
+
+
 rule run_all_combine_annotations:
     input:
         tables = expand(
             rules.bedtools_annotation_multi_intersect.output.table,
+            match_sample_path_id,
+            sample=SAMPLES,
+            path_id=PATH_IDS
+        ),
+        combined = expand(
+            rules.relabel_multi_annotation_table.output.bed,
             match_sample_path_id,
             sample=SAMPLES,
             path_id=PATH_IDS
---
+++ workflow/scripts/combine/add_annot_labels.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python3
+
+import argparse as argp
+import collections as col
+import itertools as itt
+import pathlib as pl
+
+import pandas as pd
+import xopen
+
+IVCLOSED = "left"
+
+Region = col.namedtuple("Region", ["chrom", "start", "end", "name"])
+
+
+def parse_command_line():
+
+    parser = argp.ArgumentParser()
+
+    parser.add_argument(
+        "--annotations", "--tables",
+        "-at", "-a", "-t",
+        type=lambda fp: pl.Path(fp).resolve(strict=True),
+        nargs="+",
+        required=True,
+        dest="annotation_tables"
+    )
+
+    parser.add_argument(
+        "--annotation-labels", "--labels",
+        "-l",
+        type=str,
+        nargs="+",
+        required=True,
+        dest="annotation_labels"
+    )
+
+    parser.add_argument(
+        "--intersect", "-i",
+        type=lambda fp: pl.Path(fp).resolve(strict=True),
+        required=True,
+        dest="intersect"
+    )
+
+    parser.add_argument(
+        "--drop-lowconf",
+        type=str,
+        default=["uncertain"],
+        nargs="*",
+        dest="drop_lowconf"
+    )
+
+    parser.add_argument(
+        "--output", "--out-bed",
+        "-o",
+        type=lambda fp: pl.Path(fp).resolve(strict=False),
+        dest="output",
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def check_header(filepath):
+
+    with xopen.xopen(filepath) as table:
+        first_line = table.readline().strip().split()
+        try:
+            _ = int(first_line[1])  # potential start
+            skip_rows = 0
+        except ValueError:
+            skip_rows = 1
+    return skip_rows
+
+
+def load_annotation(filepath, label, drop_lowconf):
+
+    skip_rows = check_header(filepath)
+
+    df = pd.read_csv(
+        filepath, sep="\t", header=None, skiprows=skip_rows,
+        usecols=[0,1,2,3,4,5], names=["chrom", "start", "end", "name", "score", "strand"]
+    )
+
+    if len(drop_lowconf) > 0:
+        label_bad = lambda label: any(lowconf in label for lowconf in drop_lowconf)
+        df = df.loc[
+            ~df["name"].apply(label_bad), :
+        ].copy()
+
+    #df["name"] += "|" + df["strand"]
+
+    partitions = dict()
+    for seq, sub in df.groupby("chrom"):
+        ividx = pd.IntervalIndex.from_arrays(sub["start"], sub["end"], closed=IVCLOSED)
+        s = pd.Series(sub["name"].values, index=ividx)
+        partitions[(seq, label)] = s
+    return partitions
+
+
+def prepare_annotation_files(annot_files, file_labels, drop_lowconf):
+
+    label_is_file = pl.Path(file_labels[0]).is_file()
+
+    if len(annot_files) == 1 and len(file_labels) == 1 and label_is_file:
+        # assume both listings
+        with open(annot_files[0]) as listing:
+            load_files = listing.read().strip().split()
+        with open(file_labels[0]) as listing:
+            assign_labels = listing.read().strip().split()
+    else:
+        load_files = annot_files
+        assign_labels = file_labels
+    assert len(load_files) == len(assign_labels)
+
+    label_lut = dict()
+    for annot_file, annot_label in zip(load_files, assign_labels):
+        fp = pl.Path(annot_file).resolve(strict=True)
+        ann = load_annotation(fp, annot_label, drop_lowconf)
+        label_lut.update(ann)
+
+    return label_lut
+
+
+def check_obo_errors(regions):
+
+    regions["size"] = regions["end"] - regions["start"]
+    size_one = regions.index[regions["size"] == 1]
+
+    drop_rows = []
+    new_rows = []
+    for obo_idx in size_one:
+        if obo_idx < 1:
+            continue
+        if obo_idx == regions.shape[0] - 1:
+            break
+        before_idx = obo_idx - 1
+        after_idx = obo_idx + 1
+        if regions.at[before_idx, "chrom"] != regions.at[after_idx, "chrom"]:
+            continue
+        if regions.at[before_idx, "name"] == regions.at[after_idx, "name"]:
+            chrom = regions.at[obo_idx, "chrom"]
+            start = regions.at[before_idx, "start"]
+            end = regions.at[after_idx, "end"]
+            name = regions.at[before_idx, "name"]
+            size = end - start
+            new_region = (chrom, start, end, name, size)
+
+            new_rows.append(new_region)
+
+            drop_rows.append(before_idx)
+            drop_rows.append(obo_idx)
+            drop_rows.append(after_idx)
+
+    if drop_rows:
+        regions.drop(drop_rows, axis=0, inplace=True)
+    if new_rows:
+        new_rows = pd.DataFrame.from_records(
+            new_rows, columns=["chrom", "start", "end", "name", "size"]
+        )
+        regions = pd.concat([regions, new_rows], axis=0, ignore_index=False)
+        regions.sort_values(["chrom", "start"], inplace=True)
+
+    regions.drop("size", axis=1, inplace=True)
+    regions.reset_index(drop=True, inplace=True)
+    return regions
+
+
+def main():
+
+    args = parse_command_line()
+
+    label_lut = prepare_annotation_files(
+        args.annotation_tables, args.annotation_labels,
+        args.drop_lowconf
+    )
+    intersect = pd.read_csv(args.intersect, sep="\t", header=0)
+
+    out_regions = []
+
+    for seq, subset in intersect.groupby("chrom"):
+        for row in subset.itertuples():
+            iv = pd.Interval(row.start, row.end, closed=IVCLOSED)
+            label_sources = row.list.split(",")
+            collected_labels = set()
+            for label_source in label_sources:
+                annotation = label_lut[(seq, label_source)]
+                select = annotation.index.overlaps(iv)
+                if not select.any() and len(collected_labels) == 0:
+                    # may happen if low-confidence labels
+                    # were dropped when loading annotation tables
+                    collected_labels.add("UNCERTAIN")
+                elif select.any():
+                    labels = set(annotation[select].values)
+                    collected_labels = collected_labels.union(labels)
+                else:
+                    # no overlap (e.g., low-confidence label),
+                    # but other labels were already collected
+                    # beforehand
+                    pass
+            final_label = sorted(collected_labels)
+            if not final_label:
+                final_label = "UNCERTAIN"
+            elif len(final_label) > 1:
+                final_label = ",".join(filter(lambda l: l != "UNCERTAIN", final_label))
+            else:
+                final_label = final_label[0]
+            region = Region(seq, row.start, row.end, final_label)
+            out_regions.append(region)
+
+    out_regions = sorted(out_regions)
+
+    # sanity checking
+    for a, b in itt.pairwise(out_regions):
+        if a.chrom != b.chrom:
+            continue
+        assert a.end == b.start
+
+    out_regions = pd.DataFrame.from_records(
+        out_regions, columns=["chrom", "start", "end", "name"]
+    )
+
+    out_regions = check_obo_errors(out_regions)
+    out_regions.rename({"chrom": "#chrom"}, axis=1, inplace=True)
+
+    args.output.parent.mkdir(exist_ok=True, parents=True)
+    out_regions.to_csv(args.output, sep="\t", header=True, index=False)
+
+    return 0
+
+
+
+if __name__ == "__main__":
+    main()
